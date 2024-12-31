94ebf59a8dc1c1606618dc35691126deed0b7739
unknown <Karanvir011998@gmail.com>
2024-12-31 20:41:50 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/portfolio/",
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    // exclude: ['lucide-react'],
   },
 });
