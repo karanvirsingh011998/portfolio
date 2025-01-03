73021a285e8a9af6fa9a784cabebfea4acbeace9
lz-57 <lz-57@lz-57s-Mac-mini.local>
2025-01-03 11:46:22 +0530

    added hashrouter insted of browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio">
+    {/* <BrowserRouter basename="/portfolio"> */}
+    <HashRouter basename="/">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
