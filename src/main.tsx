9a782d9c61fc446e8dcc7aa46cf92803deec6b58
karanvirsingh1998 <karanvirsingh@luminoguru.com>
2025-03-20 11:45:07 +0530

    added cursor custom

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,43 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+import AnimatedCursor from "react-animated-cursor"
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <BrowserRouter basename="/portfolio"> */}
     <HashRouter basename="/">
       <App />
+      <AnimatedCursor
+        innerSize={6}
+        outerSize={50}
+        color="255, 255, 255"
+        outerAlpha={0}
+        innerScale={1}
+        outerScale={1.5}
+        outerStyle={{
+          border: "3px solid rgba(255, 255, 255, 0.8)",
+        }}
+        clickables={[
+          "a",
+          "input[type='text']",
+          "input[type='email']",
+          "input[type='number']",
+          "input[type='submit']",
+          "input[type='image']",
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+          {
+            target: ".custom",
+
+          },
+        ]}
+      />
+
     </HashRouter>
   </StrictMode>
 );
