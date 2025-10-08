e071446322f7cbd9ff91cbeb0b0212b7fea2b613
Karanvir Singh <110287106+karanvirsingh011998@users.noreply.github.com>
2025-10-08 12:56:24 +0530

    jobTitles added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const jobTitles = [
     "Software Engineer",
     "React.js Developer",
+    "Next.js Developer",
     "Node.js Developer",
     "Fullstack Developer",
     "MERN Stack Developer",
